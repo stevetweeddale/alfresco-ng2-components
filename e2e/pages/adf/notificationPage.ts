e5dd0da29db38a224d78b65f78becb08a36c376d
Denys Vuika <denys.vuika@gmail.com>
2018-11-14 13:38:27 +0000

    try fix notification test

--- e2e/pages/adf/notificationPage.ts
+++ e2e/pages/adf/notificationPage.ts
@@ -16,7 +16,7 @@
  */
 
 import Util = require('../../util/util');
-import { element, by, protractor, browser } from 'protractor';
+import { element, by, protractor, browser, until } from 'protractor';
 
 export class NotificationPage {
 
@@ -96,8 +96,10 @@ export class NotificationPage {
     }
 
     clickNotificationButton() {
-        Util.waitUntilElementIsVisible(this.customNotificationButton);
-        this.customNotificationButton.click();
+        // Util.waitUntilElementIsVisible(this.customNotificationButton);
+        // this.customNotificationButton.click();
+        const button = browser.wait(until.elementLocated(by.css('button[data-automation-id="notification-custom-config-button"]')));
+        button.click();
     }
 
     checkActionEvent() {
